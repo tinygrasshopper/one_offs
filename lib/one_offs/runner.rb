2327dc970adbe7dbc86344f82fb7760ed089669b
Chris Rose <chrisroseuk@gmail.com>
2022-09-26 09:30:14 +0100

    Add one_off generator task

--- lib/one_offs/runner.rb
+++ lib/one_offs/runner.rb
@@ -16,7 +16,7 @@ module OneOffs
         if(!Tracker.complete?(one_off_name))
           puts "Running #{one_off}"
           require one_off
-          one_off_class = one_off_name.scan(/\d_(.*)/).flatten.first.classify
+          one_off_class = one_off_name.scan(/\d_(.*)/).flatten.first.camelize
           Object.const_get(one_off_class).send(:process)
 
           Tracker.complete(one_off_name)
--- lib/one_offs/tasks.rb
+++ lib/one_offs/tasks.rb
@@ -5,6 +5,46 @@ namespace :one_offs do
     OneOffs::Runner.run
   end
 
+  desc "Create a one off script"
+  task :create_one_off do
+    # Get command line arguments
+    ARGV.each { |a| task a.to_sym do ; end }
+
+    directory = Rails.root.join('lib', 'one_offs')
+    filename = ARGV[1]
+    time = Time.now.to_i
+
+    File.open("#{directory}/#{time}_#{filename}.rb", "w") do |file|
+      contents = "class #{filename.camelize}\n"\
+                 "  def self.process\n"\
+                 "    # Enter your migration code here....\n"\
+                 "  end\n"\
+                 "end"
+
+      file.puts(contents)
+    end
+
+    spec_directory = Rails.root.join('spec', 'lib', 'one_offs')
+
+    File.open("#{spec_directory}/#{time}_#{filename}_spec.rb", "w") do |file|
+      contents = <<~CONTENT
+      require 'one_offs/#{time}_#{filename}'
+
+      RSpec.describe #{filename.camelize} do
+        before { described_class.process }
+
+        it 'CHANGEME' do
+          expect(1).to eq(2)
+        end
+      end
+
+      CONTENT
+      file.puts(contents)
+    end
+
+    puts "Created \e[32m lib/one_offs/#{time}_#{filename}.rb \e[0m"
+    puts "Created \e[32m spec/lib/one_offs/#{time}_#{filename}_spec.rb \e[0m"
+  end
 
   desc "Create a one off tracker table"
   task :generate_tracker_table do
